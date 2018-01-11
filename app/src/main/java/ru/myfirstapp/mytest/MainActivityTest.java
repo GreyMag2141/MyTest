05f08de5dd376df25d16a775e814189a6936abd4
GreyMag2141 <grey2141@yandex.ru>
2018-01-11 21:24:14 +1000

    02

--- app/src/main/java/ru/myfirstapp/mytest/MainActivityTest.java
+++ app/src/main/java/ru/myfirstapp/mytest/MainActivityTest.java
@@ -1,28 +1,59 @@
 package ru.myfirstapp.mytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
-import android.app.ListActivity;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivityTest extends AppCompatActivity {
-    String[] tests;
-    ArrayAdapter<String> adapter;
-    ListView ListView;
-
+public class MainActivityTest extends Activity{
+    final String LOG_TAG = "myLogs";
+    ListView listView;
+    String[] names;
+    private Button button5;
+    int perehod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test);
-        ListView = findViewById(R.id.ListView);
-        tests = new String[]{"Тест 1", "Тест 2", "Тест 3"};
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests);
-        ListView.setAdapter(adapter);
+        button5 = (Button) findViewById(R.id.button);
 
+        listView = findViewById(R.id.ListView);
+                listView.setChoiceMode(listView.CHOICE_MODE_SINGLE);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.names,
+                android.R.layout.simple_list_item_single_choice);
+        listView.setAdapter(adapter);
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//              int position, long id) {
+//                Log.d(LOG_TAG, "itemClick position = " + position + " , id = " + id);
+//            }
+//        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Клик № " + position, Toast.LENGTH_SHORT).show();
+                perehod =   position;
+//                Intent intent = new Intent(MainActivityTest.this, MainActivityQuestion.class);
+//                startActivity(intent);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityTest.this, MainActivityQuestion.class);
+                intent.putExtra("perehod",perehod);
+                startActivity(intent);
+            }
+        });
     }
 }
-
 
