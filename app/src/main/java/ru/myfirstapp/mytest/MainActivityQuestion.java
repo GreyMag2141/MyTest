b50d729ef85894ea31c09c1ce20c58f7f5db6f15
GreyMag2141 <grey2141@yandex.ru>
2018-01-11 19:09:21 +1000

    01

--- app/src/main/java/ru/myfirstapp/mytest/MainActivityQuestion.java
+++ app/src/main/java/ru/myfirstapp/mytest/MainActivityQuestion.java
@@ -1,13 +1,27 @@
 package ru.myfirstapp.mytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivityQuestion extends AppCompatActivity {
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_question);
+        button3 = (Button) findViewById(R.id.button);
+        // вещаем на кнопку слушатель
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // создаем намерение, которое будет вызывать  по нажатию кнопки
+                Intent intent = new Intent(MainActivityQuestion.this, MainActivityResult.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/ru/myfirstapp/mytest/MainActivityTest.java
+++ app/src/main/java/ru/myfirstapp/mytest/MainActivityTest.java
@@ -4,21 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.app.ListActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivityTest extends AppCompatActivity {
-String[] tests;
-ArrayAdapter<String> adapter;
-ListView ListView;
+    String[] tests;
+    ArrayAdapter<String> adapter;
+    ListView ListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_test);
-
         ListView = findViewById(R.id.ListView);
-        tests  = new String[] { "Тест 1", "Тест 2", "Тест 3" };
-        adapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests);
+        tests = new String[]{"Тест 1", "Тест 2", "Тест 3"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests);
         ListView.setAdapter(adapter);
-
 
     }
 }
+
+
