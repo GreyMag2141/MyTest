faa2630f34f63f024969dbcd0ca4afe45c4181d9
GreyMag2141 <grey2141@yandex.ru>
2018-01-13 13:16:34 +1000

    03

--- app/src/main/java/ru/myfirstapp/mytest/MainActivityQuestion.java
+++ app/src/main/java/ru/myfirstapp/mytest/MainActivityQuestion.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivityQuestion extends AppCompatActivity {
     private Button button3;
@@ -12,6 +13,14 @@ public class MainActivityQuestion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent inputIntenet = getIntent();
+//        inputIntenet.getClass();
+//        inputIntenet.getExtras();
+       int position= inputIntenet.getIntExtra("position",0);
+//       textView.setText(getResources())
+//       MainActivityTest.tests[position];
+        TextView textView =findViewById(R.id.textTest);
 
         setContentView(R.layout.activity_main_question);
         button3 = (Button) findViewById(R.id.button);
